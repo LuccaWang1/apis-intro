17ebea9d3e6c1da3260176d3e83f6c27f403c284
Lucca Wang <luccawang@gmail.com>
2023-12-17 11:27:30 -0700

    More complete

--- server.py
+++ server.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template, request
 from pprint import pformat
 import os
 import requests
+import json
 
 
 app = Flask(__name__)
-app.secret_key = 'SECRETSECRETSECRET'
+app.secret_key = 'APP_SECRET'
 
 # This configuration option makes the Flask interactive debugger
 # more useful (you should remove this line in production though)
@@ -41,7 +42,7 @@ def find_afterparties():
     sort = request.args.get('sort', '')
 
     url = 'https://app.ticketmaster.com/discovery/v2/events'
-    payload = {'apikey': API_KEY}
+    payload = {'apikey': API_KEY, 'keyword': keyword, 'postalCode': postalcode, 'radius': radius, 'unit': unit, 'sort': sort}
 
     # TODO: Make a request to the Event Search endpoint to search for events
     #
@@ -54,9 +55,16 @@ def find_afterparties():
     # - Replace the empty list in `events` with the list of events from your
     #   search results
 
-    data = {'Test': ['This is just some test data'],
-            'page': {'totalElements': 1}}
-    events = []
+    # events = data['_embedded']['events']
+    res = requests.get(url, params=payload)
+    data = res.json()    
+    # data = {'keyword': ['keyword_json'],
+    #         'postalcode': {'totalElements': 1},
+    #         'radius': {'totalElements': 1},
+    #         'unit': {'totalElements': 1},
+    #         'sort': {'totalElements': 1},
+    #         }
+    events = data['_embedded']['events']
 
     return render_template('search-results.html',
                            pformat=pformat,
